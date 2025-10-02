3b394ee3b632bb57542b008d207c6d319349ab3a
Nguyễn Hà Phương Tấn <nguyenhaphuongtan@gmail.com>
2025-10-02 15:39:05 +0700

    Thêm API PUT/DELETE cho User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,17 @@
-let users = [];
-
-exports.getUsers = (req, res) => {
-    res.json(users);
+let users = []; // mảng tạm nếu chưa dùng MongoDB
+// PUT: sửa user
+exports.updateUser = (req, res) => {
+ const { id } = req.params;
+ const index = users.findIndex(u => u.id == id);
+ if (index !== -1) {
+ users[index] = { ...users[index], ...req.body };
+ res.json(users[index]);
+ } else {
+ res.status(404).json({ message: "User not found" });
+ }
 };
-
-exports.createUser = (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.status(201).json(user);
-};
+// DELETE: xóa user
+exports.deleteUser = (req, res) => {
+ const { id } = req.params;
+ users = users.filter(u => u.id != id);
+ r
