2c72f155b0d3767998f53f9dcd37eb53ae907ae5
NguyenHaPhuongTan <nguyenhaphuongtan@gmail.com>
2025-10-02 16:12:19 +0700

    Thêm API PUT/DELETE cho User

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,4 @@ router.get('/users', userController.getUsers);
 router.post('/users', userController.createUser);
 router.put('/users/:id', userController.updateUser); // PUT
 router.delete('/users/:id', userController.deleteUser); // DELETE
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 7bd3716efe8b17ca4907bac89a574f582add9748
